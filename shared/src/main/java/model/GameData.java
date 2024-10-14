3c18bdc731444c2ea7eae6e0d01016351e3abdf3
Isaac Neuenschwander <ibneuenschwander@gmail.com>
2024-10-14 17:14:14 -0600

    Delete endpoint.

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -2,6 +2,4 @@ package model;
 
 import chess.ChessGame;
 
-public record GameData(int gameID, String whiteUserName, String blackUsername,
-                       String gameName, ChessGame game) {
-}
+public record GameData(int gameID, String whiteUserName, String blackUsername, String gameName, ChessGame game) { }
