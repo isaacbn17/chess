21e3b4fd799df7d9b5abad57b922eca81e8659cf
Isaac Neuenschwander <ibneuenschwander@gmail.com>
2024-10-14 15:03:32 -0600

    Record classes.

---
+++ shared/src/main/java/model/GameData.java
@@ -0,0 +1,7 @@
+package model;
+
+import chess.ChessGame;
+
+public record GameData(int gameID, String whiteUserName, String blackUsername,
+                       String gameName, ChessGame game) {
+}
