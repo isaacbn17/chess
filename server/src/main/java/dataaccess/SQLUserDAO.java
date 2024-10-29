c2dbd0e892758e1cb6eaef5fe9672762b48cdab0
Isaac Neuenschwander <ibneuenschwander@gmail.com>
2024-10-29 15:23:23 -0600

    Execute update function with SQL

---
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -0,0 +1,81 @@
+package dataaccess;
+
+import model.UserData;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+public class SQLUserDAO implements UserDAO {
+
+    public SQLUserDAO() throws DataAccessException {
+        configureDatabase();
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS user (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `username` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` varchar(256) NOT NULL,
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`id`),
+              INDEX(username)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        String statement = "TRUNCATE pet";
+        executeUpdate(statement);
+    }
+
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i=0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) { ps.setString(i + 1, p); }
+                    else if (param instanceof Integer p) { ps.setInt(i + 1, p); }
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+                return 0;
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public UserData addUser(UserData newUser) {
+        return null;
+    }
+
+    @Override
+    public UserData getUser(String username) {
+        return null;
+    }
+}
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public interface UserDAO {
 
 
-  public void clear();
+  public void clear() throws DataAccessException;
   public UserData addUser(UserData newUser);
 
   public UserData getUser(String username);
