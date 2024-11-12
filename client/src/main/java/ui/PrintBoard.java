ab68d07112421daac745ac350f1acfed112b4a5b
Isaac Neuenschwander <ibneuenschwander@gmail.com>
2024-11-12 13:15:30 -0700

    Finished chess board rendering

--- client/src/main/java/ui/PrintBoard.java
+++ client/src/main/java/ui/PrintBoard.java
@@ -5,23 +5,35 @@ import java.nio.charset.StandardCharsets;
 import static ui.EscapeSequences.*;
 
 public class PrintBoard {
-    private static final int SQUARE_SIZE_IN_PADDED_CHARS = 1;
     private static final String[] letters = {" a", " b", " c", " d", " e", " f", " g", " h"};
     public static void main(String[] args) {
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
-
         out.print(ERASE_SCREEN);
         drawWhitePerspective(out);
+        out.print("\n");
+        drawBlackPerspective(out);
     }
 
+    private static void drawBlackPerspective(PrintStream out) {
+        drawTopOrBottomRow(out, false);
+        for (int boardRow = 1; boardRow <= 8; boardRow++) {
+            drawChessRow(out, boardRow);
+            out.print(RESET_BG_COLOR);
+            out.print("\n");
+        }
+        drawTopOrBottomRow(out, false);
+    }
+
+
+
     private static void drawWhitePerspective(PrintStream out) {
-        drawTopOrBottomRow(out);
+        drawTopOrBottomRow(out, true);
         for (int boardRow = 8; boardRow > 0; boardRow--) {
             drawChessRow(out, boardRow);
             out.print(RESET_BG_COLOR);
             out.print("\n");
         }
-        drawTopOrBottomRow(out);
+        drawTopOrBottomRow(out, true);
 
     }
 
@@ -38,14 +50,8 @@ public class PrintBoard {
 
             if (boardCol > 0 && boardCol < 9) {
                 if (boardRow == 8) {
-                    out.print(SET_TEXT_COLOR_MAGENTA);
-                    switch (boardCol) {
-                        case (1), (8) -> out.print(BLACK_ROOK);
-                        case (3), (6) -> out.print(BLACK_BISHOP);
-                        case (5) -> out.print(BLACK_KING);
-                        case (7), (2) -> out.print(BLACK_KNIGHT);
-                        case (4) -> out.print(BLACK_QUEEN);
-                    }
+                    printBackRow(out, boardCol, SET_TEXT_COLOR_MAGENTA, BLACK_ROOK, 
+                            BLACK_KNIGHT, BLACK_BISHOP, BLACK_QUEEN, BLACK_KING);
                 } else if (boardRow == 7) {
                     out.print(SET_TEXT_COLOR_MAGENTA);
                     out.print(BLACK_PAWN);
@@ -53,14 +59,8 @@ public class PrintBoard {
                     out.print(SET_TEXT_COLOR_BLUE);
                     out.print(WHITE_PAWN);
                 } else if (boardRow == 1) {
-                    out.print(SET_TEXT_COLOR_BLUE);
-                    switch (boardCol) {
-                        case (2), (7) -> out.print(WHITE_KNIGHT);
-                        case (4) -> out.print(WHITE_QUEEN);
-                        case (6), (3) -> out.print(WHITE_BISHOP);
-                        case (8), (1) -> out.print(WHITE_ROOK);
-                        case (5) -> out.print(WHITE_KING);
-                    }
+                    printBackRow(out, boardCol, SET_TEXT_COLOR_BLUE, WHITE_ROOK, 
+                            WHITE_KNIGHT, WHITE_BISHOP, WHITE_QUEEN, WHITE_KING);
                 } else {
                     out.print(EMPTY);
                 }
@@ -68,18 +68,37 @@ public class PrintBoard {
         }
     }
 
-    private static void drawTopOrBottomRow(PrintStream out) {
-//        out.print(SET_BG_COLOR_BLUE);
+    private static void printBackRow(PrintStream out, int boardCol, String textColor, String rook,
+                                      String knight, String bishop, String queen, String king) {
+        out.print(textColor);
+        switch (boardCol) {
+            case (1), (8) -> out.print(rook);
+            case (2), (7) -> out.print(knight);
+            case (3), (6) -> out.print(bishop);
+            case (4) -> out.print(queen);
+            case (5) -> out.print(king);
+        }
+    }
+
+    private static void drawTopOrBottomRow(PrintStream out, boolean colorWhite) {
+
         out.print(SET_BG_COLOR_LIGHT_GREY);
         out.print(EMPTY);
         out.print(SET_TEXT_COLOR_BLACK);
-        for (int boardCol = 0; boardCol < 8; boardCol++) {
-            out.print(letters[boardCol] + "\u3000");
+        if (colorWhite) {
+            for (int boardCol = 0; boardCol < 8; boardCol++) {
+                out.print(letters[boardCol] + "\u3000");
+            }
         }
-//        out.print(SET_BG_COLOR_YELLOW);
+        else {
+            for (int boardCol = 7; boardCol >= 0; boardCol--) {
+                out.print(letters[boardCol] + "\u3000");
+            }
+        }
         out.print(EMPTY);
         out.print(RESET_BG_COLOR);
         out.print("\n");
     }
+
 
 }
