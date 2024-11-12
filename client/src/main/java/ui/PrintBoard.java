ec7b8f1c12c457cc99b85de881d0187b02c3f5c2
Isaac Neuenschwander <ibneuenschwander@gmail.com>
2024-11-11 19:38:21 -0700

    More work on printing the game

--- client/src/main/java/ui/PrintBoard.java
+++ client/src/main/java/ui/PrintBoard.java
@@ -5,7 +5,6 @@ import java.nio.charset.StandardCharsets;
 import static ui.EscapeSequences.*;
 
 public class PrintBoard {
-    private static final int BOARD_SIZE_IN_SQUARES = 10;
     private static final int SQUARE_SIZE_IN_PADDED_CHARS = 1;
     private static final String[] letters = {" a ", " b ", " c ", " d ", " e ", " f ", " g ", " h "};
     private static final String[] pieces = {BLACK_ROOK, BLACK_KNIGHT, BLACK_BISHOP, BLACK_QUEEN,
@@ -15,23 +14,12 @@ public class PrintBoard {
 
         out.print(ERASE_SCREEN);
         drawWhitePerspective(out);
-
-        out.print(SET_BG_COLOR_BLACK);
-        out.print(SET_TEXT_COLOR_WHITE);
     }
 
     private static void drawWhitePerspective(PrintStream out) {
         drawTopOrBottomRow(out);
         for (int boardRow = 8; boardRow > 0; boardRow--) {
-            out.print(SET_BG_COLOR_LIGHT_GREY);
-            out.print(SET_TEXT_COLOR_BLACK);
-            out.printf(" %s ", boardRow);
-
-            drawChessRows(out, boardRow);
-
-            out.print(SET_BG_COLOR_LIGHT_GREY);
-            out.print(SET_TEXT_COLOR_BLACK);
-            out.printf(" %s ", boardRow);
+            drawChessRow(out, boardRow);
             out.print(RESET_BG_COLOR);
             out.print("\n");
         }
@@ -39,17 +27,77 @@ public class PrintBoard {
 
     }
 
-    private static void drawChessRows(PrintStream out, int boardRow) {
-        for (int boardCol = 1; boardCol < 9; boardCol++) {
-            if ((boardCol + boardRow) % 2 == 1) {
+    private static void drawChessRow(PrintStream out, int boardRow) {
+        for (int boardCol = 0; boardCol < 10; boardCol++) {
+            if (boardCol == 0 || boardCol == 9) {
+                out.print(SET_BG_COLOR_LIGHT_GREY);
+                out.print(SET_TEXT_COLOR_BLACK);
+                out.printf(" %s ", boardRow);
+            }
+            else if ((boardCol + boardRow) % 2 == 1) {
                 out.print(SET_BG_COLOR_WHITE);
-                out.print(SET_TEXT_COLOR_MAGENTA);
-                out.print(BLACK_ROOK);
+
+                if (boardRow == 8) {
+                    out.print(SET_TEXT_COLOR_MAGENTA);
+                    switch (boardCol) {
+                        case(1) -> out.print(BLACK_ROOK);
+                        case(3) -> out.print(BLACK_BISHOP);
+                        case(5) -> out.print(BLACK_KING);
+                        case(7) -> out.print(BLACK_KNIGHT);
+                    }
+                }
+                else if (boardRow == 7) {
+                    out.print(SET_TEXT_COLOR_MAGENTA);
+                    out.print(BLACK_PAWN);
+                }
+                else if (boardRow == 2) {
+                    out.print(SET_TEXT_COLOR_BLUE);
+                    out.print(WHITE_PAWN);
+                }
+                else if (boardRow == 1) {
+                    out.print(SET_TEXT_COLOR_BLUE);
+                    switch (boardCol) {
+                        case(2) -> out.print(WHITE_KNIGHT);
+                        case(4) -> out.print(WHITE_QUEEN);
+                        case(6) -> out.print(WHITE_BISHOP);
+                        case(8) -> out.print(WHITE_ROOK);
+                    }
+                }
+                else {
+                    out.print(EMPTY);
+                }
             }
             else {
                 out.print(SET_BG_COLOR_BLACK);
-                out.print(SET_TEXT_COLOR_MAGENTA);
-                out.print(BLACK_ROOK);
+                switch (boardRow) {
+                    case (8) -> {
+                        out.print(SET_TEXT_COLOR_MAGENTA);
+                        switch (boardCol) {
+                            case(2) -> out.print(BLACK_KNIGHT);
+                            case(4) -> out.print(BLACK_QUEEN);
+                            case(6) -> out.print(BLACK_BISHOP);
+                            case(8) -> out.print(BLACK_ROOK);
+                        }
+                    }
+                    case (7) -> {
+                        out.print(SET_TEXT_COLOR_MAGENTA);
+                        out.print(BLACK_PAWN);
+                    }
+                    case (2) -> {
+                        out.print(SET_TEXT_COLOR_BLUE);
+                        out.print(WHITE_PAWN);
+                    }
+                    case (1) -> {
+                        out.print(SET_TEXT_COLOR_BLUE);
+                        switch (boardCol) {
+                            case(1) -> out.print(WHITE_ROOK);
+                            case(3) -> out.print(WHITE_BISHOP);
+                            case(5) -> out.print(WHITE_KING);
+                            case(7) -> out.print(WHITE_KNIGHT);
+                        }
+                    }
+                    default -> out.print(EMPTY);
+                }
             }
         }
     }
