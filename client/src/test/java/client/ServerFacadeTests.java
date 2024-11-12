5bcbfe8c46766c137270359f757aa86a48d76498
Isaac Neuenschwander <ibneuenschwander@gmail.com>
2024-11-12 16:02:17 -0700

    ServerFacade test for create, join, and list games.

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -7,6 +7,8 @@ import server.Server;
 import server.ServerFacade;
 import service.DeleteService;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -14,7 +16,6 @@ public class ServerFacadeTests {
 
     private static Server server;
     static ServerFacade facade;
-    private static String authToken = "";
 
     @BeforeAll
     public static void init() throws DataAccessException {
@@ -36,14 +37,15 @@ public class ServerFacadeTests {
     }
 
     @AfterAll
-    static void stopServer() {
+    static void stopServer() throws DataAccessException {
         server.stop();
+        clearDatabase();
     }
 
 
     @Test
     public void register() throws Exception {
-        RegisterResult authData = facade.createUser(new UserData("player1", "password", "hello@gmail.com"));
+        RegisterResult authData = facade.createUser(new UserData("player", "password", "hello@gmail.com"));
         assertTrue(authData.authToken().length() > 10);
     }
     @Test
@@ -52,8 +54,7 @@ public class ServerFacadeTests {
     }
     @Test
     public void login() throws Exception {
-        RegisterResult authData = facade.loginUser(new LoginRequest("player1", "password"));
-        authToken = authData.authToken();
+        RegisterResult authData = facade.loginUser(new LoginRequest("player", "password"));
         assertTrue(authData.authToken().length() > 10);
     }
     @Test
@@ -62,20 +63,48 @@ public class ServerFacadeTests {
     }
     @Test
     public void logout() throws Exception {
+        RegisterResult authData = facade.createUser(new UserData("player1", "pw", "hello"));
+        String authToken = authData.authToken();
         facade.logoutUser(authToken);
+        assertThrows(Exception.class, () -> facade.createGame(new GameName("hello"), authToken).toString());
     }
     @Test
-    public void logoutFails() throws Exception {
+    public void logoutFails() {
         assertThrows(Exception.class, () -> facade.logoutUser("Not an authToken"));
     }
-//    @Test
-//    public void createGame() throws Exception {
-//        RegisterResult authData = facade.createUser(new UserData("player1", "password", "hello@gmail.com"));
-//        assertTrue(authData.authToken().length() > 10);
-//    }
-//    @Test
-//    public void createGameFails() throws Exception {
-//        assertThrows(Exception.class, () -> facade.createUser(new UserData("", "", "")));
-//    }
-
+    @Test
+    public void createGame() throws Exception {
+        RegisterResult authData = facade.createUser(new UserData("player3", "password", "hello@gmail.com"));
+        GameID gameID = facade.createGame(new GameName("Game10"), authData.authToken());
+        assertNotNull(gameID);
+    }
+    @Test
+    public void createGameFails() {
+        assertThrows(Exception.class, () -> facade.createGame(new GameName("hello"), "Not an authToken"));
+    }
+    @Test
+    public void listGames() throws Exception {
+        RegisterResult authData = facade.createUser(new UserData("player4", "pw", "mail.com"));
+        facade.createGame(new GameName("Game10"), authData.authToken());
+        ArrayList<GameSimplified> expected = facade.listGames(authData.authToken());
+        assertFalse(expected.isEmpty());
+    }
+    @Test
+    public void listGamesFails() {
+        assertThrows(Exception.class, () -> facade.listGames("Not an authToken"));
+    }
+    @Test
+    public void joinGame() throws Exception {
+        RegisterResult authData = facade.createUser(new UserData("player5", "pw", "mail"));
+        facade.createGame(new GameName("Game10"), authData.authToken());
+        facade.joinGame(new JoinRequest("white", 1), authData.authToken());
+        ArrayList<GameSimplified> games = facade.listGames(authData.authToken());
+        String whiteUsername = games.getFirst().whiteUsername();
+        assertNotNull(whiteUsername);
+    }
+    @Test
+    public void joinGames() {
+        assertThrows(Exception.class,
+                () -> facade.joinGame(new JoinRequest("WHITE", 2), "hello"));
+    }
 }
